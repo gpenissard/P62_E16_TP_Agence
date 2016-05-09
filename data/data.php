63f9c91dfca358581d37e2744584a4c3c126eef9
gpenissard <gilles.penissard@isi-mtl.com>
2016-05-09 10:54:39 -0400

    Création tableau des données

---
+++ data/data.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Tableau principal des données du site
+ * (suivant le projet) Chaque item doit contenir au minimul les champs suivants
+ * - nom
+ * - categorie
+ * - etc
+ */
+
+$data = array(
+    0 => array(
+        'nom' => 'Trekking',
+        'categorie' => 'aventure',
+        'prix' => 1899.99,
+        'photo' => 'toto.jpg',
+        'dans_carousel' => true,
+    ),
+    1 => array(
+        'nom' => 'Robots',
+        'categorie' => 'science-fiction',
+        'prix' => 489.99,
+        'photo' => 'kiki.jpg',
+        'dans_carousel' => false,
+    ),
+    2 => array(
+        'nom' => 'Football',
+        'categorie' => 'sport',
+        'prix' => 88.00,
+        'photo' => 'foot.jpg',
+        'dans_carousel' => true,
+    ),
+);
